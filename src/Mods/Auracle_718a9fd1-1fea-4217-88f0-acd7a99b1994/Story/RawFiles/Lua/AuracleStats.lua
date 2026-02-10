71636ac35f057fe21552db564dfac924ffda9287
Ryan Rowland <ryan.rowland@gmail.com>
2026-02-10 12:19:59 +0000

    fix: convert aura_likes to dict not tuple array

--- src/Mods/Auracle_718a9fd1-1fea-4217-88f0-acd7a99b1994/Story/RawFiles/Lua/AuracleStats.lua
+++ src/Mods/Auracle_718a9fd1-1fea-4217-88f0-acd7a99b1994/Story/RawFiles/Lua/AuracleStats.lua
@@ -1,7 +1,9 @@
 local AURA_RANGE = 30
 local AURAS = {"GUARDIAN_ANGEL_AURA", "FAVOURABLE_WIND_AURA", "EVASION_AURA",
                "FIRE_BRAND_AURA", "VENOM_AURA", "VAMPIRISM_AURA"}
-local AURA_LIKES = {{"Target_MasterOfSparks", "AreaRadius"}}
+local AURA_LIKES = {
+    Target_MasterOfSparks = "AreaRadius"
+}
 
 local function UpdateAuraRange(statName, rangeField)
     rangeField = rangeField or "AuraRadius"
@@ -9,14 +11,14 @@ local function UpdateAuraRange(statName, rangeField)
 end
 
 function AuracleStatUpdates(event)
-    -- Leadership
+
     Ext.ExtraData["LeadershipRange"] = AURA_RANGE
-    -- Aura
+
     for _, aura in pairs(AURAS) do
         UpdateAuraRange(aura)
     end
-    -- Aura-Like
-    for _, auraLike in pairs(AURA_LIKES) do
-        UpdateAuraRange(auraLike[0], auraLike[1])
+
+    for auraLike, rangeField in pairs(AURA_LIKES) do
+        UpdateAuraRange(auraLike, rangeField)
     end
 end
