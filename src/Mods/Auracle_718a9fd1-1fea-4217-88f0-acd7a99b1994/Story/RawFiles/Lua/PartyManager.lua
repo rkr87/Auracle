bb6cc8c91142711d2b46bed074bbe6334f7bbbff
Ryan Rowland <ryan.rowland@gmail.com>
2026-02-10 10:40:22 +0000

    refactor: simplify party member logic

--- src/Mods/Auracle_718a9fd1-1fea-4217-88f0-acd7a99b1994/Story/RawFiles/Lua/PartyManager.lua
+++ src/Mods/Auracle_718a9fd1-1fea-4217-88f0-acd7a99b1994/Story/RawFiles/Lua/PartyManager.lua
@@ -2,30 +2,25 @@ Ext.Require("CharacterTools.lua")
 Ext.Require("Constants.lua")
 Ext.Require("Helpers.lua")
 
-local partyMembers = {}
-local bestLeadership = -1
-
-function AddPartyMember(character_uuid)
-    local character = Ext.Entity.GetCharacter(character_uuid)
-    local leadership = GetCharacterLeadership(character)
-
-    partyMembers[character_uuid] = {
-        guid = character.MyGuid,
-        leadership = leadership,
-        hasStacks = HasActiveStatus(character.MyGuid, STATUS_STACKABLE_STATS) > 0
+function GetPartyMember(char_uuid)
+    local char = Ext.Entity.GetCharacter(char_uuid)
+    return {
+        guid = char.MyGuid,
+        leadership = GetCharacterLeadership(char),
+        hasStacks = HasActiveStatus(char.MyGuid, STATUS_STACKABLE_STATS) > 0
     }
-
-    if leadership > bestLeadership then
-        bestLeadership = leadership
-    end
 end
 
 function GetPartyMembers()
-    partyMembers = {}
-    bestLeadership = -1
+    local partyMembers = {}
+    local bestLeadership = -1
     local dbPlayers = Osi.DB_IsPlayer:Get(nil)
     for _, dbChar in pairs(dbPlayers) do
-        AddPartyMember(dbChar[1])
+        local member = GetPartyMember(dbChar[1])
+        partyMembers[dbChar[1]] = member
+        if member.leadership > bestLeadership then
+            bestLeadership = member.leadership
+        end
     end
     return partyMembers, bestLeadership
 end
